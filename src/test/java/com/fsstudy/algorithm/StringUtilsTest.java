cc26908302cff5d22855c1a52747ff013719d65e
zhangfengshang <zhangfengshang@goodo-tech.com>
2025-09-17 10:28:42 +0800

    test:新增测试文件

---
+++ src/test/java/com/fsstudy/algorithm/StringUtilsTest.java
@@ -0,0 +1,11 @@
+package com.fsstudy.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+public class StringUtilsTest {
+    @Test
+    public void testSubString() {
+        String a = "";
+        System.out.println(a.substring(0, 0));
+    }
+}
